a3554178777ccb0dd4667478d2fa17911703e61c
Max M <max@maxmue.eu>
2023-03-30 21:40:59 +0200

    add TODO's for devcontainer-setup

--- .devcontainer/setup.sh
+++ .devcontainer/setup.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )/../" >/dev/null 2>&1 && pwd )"
 
+# TODO: this chown breaks the ownership on the development machine
 chown -R 33 $DIR
 
 git config --global --add safe.directory $DIR
@@ -13,4 +14,7 @@ git submodule update --init
 cp .devcontainer/codespace.config.php config/codespace.config.php
 
 # Nextcloud installation
+# TODO: the first setup works, but afterwards APCu has issues:
+# Memcache \OC\Memcache\APCu not available for local cache (Is the matching PHP module installed and enabled?
+# TODO: data directory inside webroot -> do we want that?
 sudo -uwww-data php $DIR/occ maintenance:install --admin-pass admin
