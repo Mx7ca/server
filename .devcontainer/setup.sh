1de01d960031a217dba0e6db5c966aaa54265703
Max M <max@maxmue.eu>
2023-03-30 21:35:22 +0200

    fix devcontainer, setup nextcloud installation with apache2

--- .devcontainer/setup.sh
+++ .devcontainer/setup.sh
@@ -1,8 +1,16 @@
 #!/bin/bash
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )/../" >/dev/null 2>&1 && pwd )"
 
+chown -R 33 $DIR
+
+git config --global --add safe.directory $DIR
+git config --global --add safe.directory $DIR/3rdparty
+
 cd $DIR/
 git submodule update --init
 
 # Codespace config
 cp .devcontainer/codespace.config.php config/codespace.config.php
+
+# Nextcloud installation
+sudo -uwww-data php $DIR/occ maintenance:install --admin-pass admin
